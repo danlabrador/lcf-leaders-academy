74a7cd8ef6f4dd8aeec3465ec33be7f72c8d4de6
Dan Labrador <dan.labrador@myamazonguy.com>
2024-08-18 16:03:58 -0400

    feat: create seed.ts

--- src/env.ts
+++ src/env.ts
@@ -6,10 +6,20 @@ dotenv.config();
 
 // Define schema for environment variables
 const envSchema = z.object({
+  NODE_ENV: z.string(),
+  DEV_DATABASE_URL: z.string().min(1),
+  PROD_DATABASE_URL: z.string().min(1),
+  HOST_NAME: z.string().min(1),
+  EMAIL_FROM: z.string().min(1),
+  EMAIL_SERVER_HOST: z.string().min(1),
+  EMAIL_SERVER_PORT: z.string().min(1),
+  EMAIL_SERVER_USER: z.string().min(1),
+  EMAIL_SERVER_PASSWORD: z.string().min(1),
   CLOUDFLARE_ACCOUNT_ID: z.string().min(1),
   CLOUDFLARE_ACCESS_KEY_ID: z.string().min(1),
   CLOUDFLARE_SECRET_ACCESS_KEY: z.string().min(1),
   CLOUDFLARE_BUCKET_NAME: z.string().min(1),
+  RESEND_AUDIENCE_ID: z.string().min(1),
 });
 
 // Validate environment variables
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,4 @@
 import express from "express";
-import remindersRouter from "./routers/reminders";
-import { formatDate } from "./util/date";
-import { env } from "./env";
-import { createULID } from "./util/ulid";
 
 const app = express();
 app.use(express.json());
-app.use("/reminders", remindersRouter);
-
-app.get("/", (req, res) => {
-  res.json({
-    date: formatDate(new Date()),
-  });
-});
-
-app.listen(8000, () => console.log("Server is running on port 8000"));
