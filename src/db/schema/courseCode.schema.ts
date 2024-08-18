f63d3b54dce03f81efa0c28e23a7542838b47cd0
Dan Labrador <dan.labrador@myamazonguy.com>
2024-08-18 11:00:34 -0400

    feat: Add alternateTo field to CourseCode schema

--- src/db/schema/courseCode.schema.ts
+++ src/db/schema/courseCode.schema.ts
@@ -8,6 +8,7 @@ const CourseCodeSchema = new Schema(
     code: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     description: { type: String },
+    alternateTo: { type: String, ref: "CourseCode" },
 
     // Miscellanous
     createdBy: { type: String, ref: "User" },
